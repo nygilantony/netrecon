981f888b1b8886c098ca42b1b8e01b0e56f4e82b
Nygil Antony <nygilantony9@gmail.com>
2026-04-08 12:15:29 -0400

    Add files via upload

---
+++ utils/services.py
@@ -0,0 +1,17 @@
+def identify(port):
+    common = {
+        21: "FTP",
+        22: "SSH",
+        23: "TELNET",
+        25: "SMTP",
+        53: "DNS",
+        80: "HTTP",
+        110: "POP3",
+        139: "NETBIOS",
+        143: "IMAP",
+        443: "HTTPS",
+        445: "SMB",
+        3306: "MYSQL",
+        3389: "RDP"
+    }
+    return common.get(port, "UNKNOWN")
---
+++ utils/ui.py
@@ -0,0 +1,11 @@
+def banner(text):
+    print(text)
+
+def info(msg):
+    print(msg)
+
+def warn(msg):
+    print(msg)
+
+def spacer(n=1):
+    print("\n" * n)
